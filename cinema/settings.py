bfa3ed3ef9a0a1c9bfd47e3fa87f443e9bbd3436
Hilario <hilario@wisphub.net>
2023-08-05 13:58:37 -0500

    Correcciones con dominio djmovie.com

--- cinema/settings.py
+++ cinema/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = django_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', 'djmovie.com']
 
 
 # Application definition
